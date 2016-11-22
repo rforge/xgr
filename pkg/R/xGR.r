368b54eafe79184825c21eb69ac924c56c7b8b55
hfang <hfang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-22 23:31:58 +0000

    Update to the version 1.0.5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xgr@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/xGR.r
+++ pkg/R/xGR.r
@@ -3,7 +3,7 @@
 #' \code{xGR} is supposed to create a GRanges object given a list of genomic regions. 
 #'
 #' @param data input genomic regions (GR). If formatted as "chr:start-end" (see the next parameter 'format' below), GR should be provided as a vector in the format of 'chrN:start-end', where N is either 1-22 or X, start (or end) is genomic positional number; for example, 'chr1:13-20'. If formatted as a 'data.frame', the first three columns correspond to the chromosome (1st column), the starting chromosome position (2nd column), and the ending chromosome position (3rd column). If the format is indicated as 'bed' (browser extensible data), the same as 'data.frame' format but the position is 0-based offset from chromomose position. If the genomic regions provided are not ranged but only the single position, the ending chromosome position (3rd column) is allowed not to be provided. The data could also be an object of 'GRanges' (in this case, formatted as 'GRanges')
-#' @param format the format of the input data. It can be one of "data.frame", "chr:start-end", "bed" or "GRanges"
+#' @param format the format of the input data. It can be one of "chr:start-end", "data.frame", "bed" or "GRanges"
 #' @param build.conversion the conversion from one genome build to another. The conversions supported are "hg38.to.hg19" and "hg18.to.hg19". By default it is NA (no need to do so)
 #' @param verbose logical to indicate whether the messages will be displayed in the screen. By default, it sets to true for display
 #' @param RData.location the characters to tell the location of built-in RData files. See \code{\link{xRDataLoader}} for details
@@ -44,8 +44,12 @@ xGR <- function(data, format=c("chr:start-end","data.frame","bed","GRanges"), bu
         data <- data
     }else if(!is.null(data) & any(!is.na(data))){
     	if(length(data)==1){
-			data <- utils::read.delim(file=data, header=F, row.names=NULL, stringsAsFactors=F)
-			data <- unique(data[,1])
+    		if(file.exists(data)){
+    			data <- utils::read.delim(file=data, header=F, row.names=NULL, stringsAsFactors=F)
+    			data <- unique(data[,1])
+    		}else{
+				data <- data
+			}
 		}else{
 			data <- data
 		}
@@ -65,11 +69,10 @@ xGR <- function(data, format=c("chr:start-end","data.frame","bed","GRanges"), bu
 		}
 		## make sure positions are numeric
 		ind <- suppressWarnings(which(!is.na(as.numeric(data[,2])) & !is.na(as.numeric(data[,3]))))
-		data <- data[ind,]
 		dGR <- GenomicRanges::GRanges(
-			seqnames=S4Vectors::Rle(data[,1]),
-			ranges = IRanges::IRanges(start=as.numeric(data[,2]), end=as.numeric(data[,3])),
-			strand = S4Vectors::Rle(rep('*',nrow(data)))
+			seqnames=S4Vectors::Rle(data[ind,1]),
+			ranges = IRanges::IRanges(start=as.numeric(data[ind,2]), end=as.numeric(data[ind,3])),
+			strand = S4Vectors::Rle(rep('*',length(ind)))
 		)
 		names(dGR) <- paste(data[,1], ':', data[,2], '-', data[,3], sep='')
 		
@@ -77,19 +80,18 @@ xGR <- function(data, format=c("chr:start-end","data.frame","bed","GRanges"), bu
 		data <- unique(data)
 		input <- do.call(rbind, strsplit(data, ":|-"))
 		if(ncol(input)>=3){
-			data <- input[,1:3]
+			data <- matrix(input[,1:3], nrow=nrow(input))
 		}else if(ncol(input)==2){
-			data <- input[,c(1,2,2)]
+			data <- matrix(input[,c(1,2,2)], nrow=nrow(input))
 		}else{
 			stop("Your input 'data' does not meet the format 'chr:start-end'!\n")
 		}
 		## make sure positions are numeric
 		ind <- suppressWarnings(which(!is.na(as.numeric(data[,2])) & !is.na(as.numeric(data[,3]))))
-		data <- data[ind,]
 		dGR <- GenomicRanges::GRanges(
-			seqnames=S4Vectors::Rle(data[,1]),
-			ranges = IRanges::IRanges(start=as.numeric(data[,2]), end=as.numeric(data[,3])),
-			strand = S4Vectors::Rle(rep('*',nrow(data)))
+			seqnames=S4Vectors::Rle(data[ind,1]),
+			ranges = IRanges::IRanges(start=as.numeric(data[ind,2]), end=as.numeric(data[ind,3])),
+			strand = S4Vectors::Rle(rep('*',length(ind)))
 		)
 		names(dGR) <- paste(data[,1], ':', data[,2], '-', data[,3], sep='')
 		
@@ -97,11 +99,10 @@ xGR <- function(data, format=c("chr:start-end","data.frame","bed","GRanges"), bu
 		## construct data GR
 		## make sure positions are numeric
 		ind <- suppressWarnings(which(!is.na(as.numeric(data[,2])) & !is.na(as.numeric(data[,3]))))
-		data <- data[ind,]
 		dGR <- GenomicRanges::GRanges(
-			seqnames=S4Vectors::Rle(data[,1]),
-			ranges = IRanges::IRanges(start=as.numeric(data[,2])+1, end=as.numeric(data[,3])),
-			strand = S4Vectors::Rle(rep('*',nrow(data)))
+			seqnames=S4Vectors::Rle(data[ind,1]),
+			ranges = IRanges::IRanges(start=as.numeric(data[ind,2])+1, end=as.numeric(data[ind,3])),
+			strand = S4Vectors::Rle(rep('*',length(ind)))
 		)
 		names(dGR) <- paste(data[,1], ':', data[,2], '-', data[,3], sep='')
 	}else if(format=="GRanges"){
