99f5d4ec33691433cda8e2661ac4d857d39b80b1
hfang <hfang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-29 20:36:57 +0000

    Update to the version 1.1.5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xgr@234 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/xFunArgs.r
+++ pkg/R/xFunArgs.r
@@ -3,10 +3,10 @@
 #' \code{xFunArgs} is supposed to assign (and evaluate) arguments with default values for a given function.
 #'
 #' @param fun character specifying the name of the function
-#' @param action logical to indicate whether the function will act as it should be (with assigned values in the current environment). By default, it sets to FALSE
+#' @param action logical to indicate whether the function will act as it should be (with assigned values in the current environment). By default, it sets to NULL, return a string specifying the assignment to be evalated
 #' @param verbose logical to indicate whether the messages will be displayed in the screen. By default, it sets to TRUE for display
 #' @return
-#' a list containing arguments and their default values
+#' If action is logical, a list containing arguments and their default values. If action is NULL, a string specifying the assignment to be evalated.
 #' @note
 #' This function is potentially useful when debugging as it frees developers from specifying default values for all arguments except those arguments of interest
 #' @export
@@ -15,22 +15,39 @@
 #' @examples
 #' xFunArgs(fun="xRDataLoader")
 
-xFunArgs <- function(fun, action=F, verbose=TRUE)
+xFunArgs <- function(fun, action=NULL, verbose=TRUE)
 {
     
     args_list <- base::formals(fun)
     args_names <- names(args_list)
-    for(i in 1:length(args_list)){
-        lft <- args_names[[i]]
-        rgt <- paste(base::deparse(args_list[[i]]),collapse='')
-        if(rgt!=''){
-            tmp <- paste(lft, '<-', rgt, sep=' ')
-            if(action==T){
-                base::eval(base::parse(text=tmp), envir=parent.frame())
-            }else{
-                base::eval(base::parse(text=tmp))
-            }
-        }
+    
+    if(is.null(action)){
+    	
+    	ls_vec <- lapply(1:length(args_list), function(i){
+    		lft <- args_names[[i]]
+    		rgt <- paste(base::deparse(args_list[[i]]),collapse='')
+    		if(rgt!=''){
+    			paste0(lft, '=', rgt, '[1];')
+    		}else{
+    			NULL	
+    		}
+    	})
+    	res <- noquote(paste(unlist(ls_vec), collapse=''))
+    	return(res)
+    	
+    }else{
+		for(i in 1:length(args_list)){
+			lft <- args_names[[i]]
+			rgt <- paste(base::deparse(args_list[[i]]),collapse='')
+			if(rgt!=''){
+				tmp <- paste(lft, '<-', rgt, sep=' ')
+				if(action==T){
+					base::eval(base::parse(text=tmp), envir=parent.frame())
+				}else if(action==F){
+					base::eval(base::parse(text=tmp))
+				}
+			}
+		}
     }
     
     if(verbose){
